33a920d6bea8dc77c23a8f671c7f35cdc5c383c3
Liliana2201 <subinaliliana@gmail.com>
2023-12-08 17:06:28 +0700

    Десериализация JSON-текста в объекты модели

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.squareup.retrofit2:retrofit:2.5.0")
     implementation("com.squareup.retrofit2:converter-scalars:2.5.0")
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("com.squareup.retrofit2:converter-gson:2.4.0")
 }
--- app/src/main/java/api/FlickrApi.kt
+++ app/src/main/java/api/FlickrApi.kt
@@ -11,6 +11,6 @@ interface FlickrApi {
                 "&nojsoncallback=1" +
                 "&extras=url_s"
     )
-    fun fetchPhotos(): Call<String>
+    fun fetchPhotos(): Call<FlickrResponse>
 
 }
